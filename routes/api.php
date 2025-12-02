a47d0a59396053747d79577bf77f8c4fa9ae6fc1
ASNProject <asnproject02@gmail.com>
2025-12-02 09:25:19 +0700

    update endpoint name

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/vehicle-maps', VehicleMapsController::class);
+Route::apiResource('/tire-pressure', VehicleMapsController::class);
